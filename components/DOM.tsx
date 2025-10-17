084d67679f31460b1cce776339a222cf93ce095b
Quentin Brohan <brohan.quentin@gmail.com>
2025-10-17 19:11:01 +0200

    feat(header): add gsap motion

--- components/DOM.tsx
+++ components/DOM.tsx
@@ -5,7 +5,8 @@ import { gsap } from 'gsap'
 import { useGSAP } from '@gsap/react'
 
 const DOM: React.FC = () => {
-    const [showAbout, setShowAbout] = useState(false)
+    const [showAboutPanel, setShowAboutPanel] = useState(false)
+
     const containerRef = useRef<HTMLElement>(null)
     const aboutButtonRef = useRef<HTMLButtonElement>(null)
     const aboutPanelRef = useRef<HTMLDivElement>(null)
@@ -25,24 +26,28 @@ const DOM: React.FC = () => {
             y: 32,
             duration: 0.8,
             stagger: 0.2,
-        }), '<+=1.2')
+        }), '<+=0.6')
             .add(
 
                 gsap.to(helperEls, {
                     scrambleText: {
                         text: '{original}',
                         chars: 'DAOTSHCN5-',
-                        revealDelay: 0.5,
+                        revealDelay: 0.4,
                     },
                     stagger: 0.2,
                     repeat: -1,
                     repeatDelay: 5,
+                    duration: 1
                 }), '<')
-            .add(gsap.from(aboutButtonRef.current, {
-                opacity: 0,
-                y: 32,
-                duration: 0.3,
-            }), '<+=0.6')
+            .add(gsap.fromTo(aboutButtonRef.current,
+                { autoAlpha: 0, y: 32 },
+                {
+                    autoAlpha: 1,
+                    y: 0,
+                    duration: 0.3,
+                }
+            ), '<+=0.6')
 
     }, { scope: containerRef })
 
@@ -54,46 +59,49 @@ const DOM: React.FC = () => {
 
             tl.fromTo(
                 aboutPanelRef.current,
-                { opacity: 0, y: 32, visibility: 'visible' },
+                { opacity: 0, y: 32, visibility: 'hidden' },
                 {
                     opacity: 1, y: 0, duration: 0.5,
+                    autoAlpha: 1,
+                    visibility: 'visible'
                 }
             )
 
             aboutTlRef.current = tl
         }
 
-        if (showAbout) {
+        if (showAboutPanel) {
             aboutTlRef.current.play()
         } else {
             aboutTlRef.current.reverse()
         }
-    }, [showAbout])
+    }, [showAboutPanel])
+
     return (
-        <main ref={containerRef} className="relative h-screen w-full pointer-events-none z-1">
-            <div className='invisible md:visible' >
-                <p data-helper className="absolute left-4 top-1/2 text-white/70 uppercase text-xs">
+        <main ref={containerRef} className="relative h-screen w-full z-1">
+            <div className='invisible md:visible pointer-events-none' >
+                <p data-helper className="absolute left-4 top-1/3 text-white/70 uppercase text-xs">
                     [ Click on a monitor to view fullscreen ]
                 </p>
-                <p data-helper className="absolute right-4 bottom-8 text-white/70 uppercase text-xs">
+                <p data-helper className="absolute right-4 bottom-1/3 text-white/70 uppercase text-xs">
                     [ Scroll to get closer ]
                 </p>
-                <p data-helper className="absolute right-4 bottom-4 text-white/70 uppercase text-xs">
+                <p data-helper className="absolute right-4 bottom-1/6 text-white/70 uppercase text-xs">
                     [ Move your mouse to rotate ]
                 </p>
             </div>
 
             <button
                 ref={aboutButtonRef}
-                onClick={() => setShowAbout(!showAbout)}
+                onClick={() => setShowAboutPanel(!showAboutPanel)}
                 className="absolute left-4 bottom-4 pointer-events-auto text-white hover:opacity-60! transition cursor-pointer text-xs uppercase z-1"
             >
-                {showAbout ? '( Close )' : '( About )'}
+                {showAboutPanel ? '( Close )' : '( About )'}
             </button>
 
             <div
                 ref={aboutPanelRef}
-                className="absolute left-4 bottom-[calc(1rem+2rem)] pointer-events-auto text-white max-w-md flex flex-col gap-2 invisible"
+                className="absolute left-4 bottom-[calc(1rem+2rem)] pointer-events-auto text-white max-w-md flex flex-col gap-1"
             >
                 <p className="text-xs">
                     Visualizing hormones with procedural shaders: dopamine, oxytocin, serotonin. R3F.
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,9 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import React from 'react';
+import React, { useRef } from 'react';
+import { gsap } from 'gsap'
+import { useGSAP } from '@gsap/react'
 
 const NAV_LINKS = [{
     href: "/dopamine",
@@ -23,18 +25,51 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ }) => {
     const pathname = usePathname();
 
+    const containerRef = useRef<HTMLElement>(null)
+
+    useGSAP(() => {
+        const tl = gsap.timeline()
+
+        const container = gsap.utils.selector(containerRef)
+
+        const [mainLinkEl, ...navLinkEls] = container('a')
+        const splitEls = container('span')
+
+        tl.add(gsap.fromTo(mainLinkEl, {
+            opacity: 0,
+            y: 50,
+        }, {
+            opacity: 1,
+            y: 0,
+        }), '<+=1.2')
+            .add(
+                gsap.fromTo([navLinkEls, splitEls], {
+                    opacity: 0,
+                    y: 32,
+
+                }, {
+                    opacity: 1,
+                    stagger: 0.1,
+                }), '<+=0.25')
+
+
+
+    }, {
+        scope: containerRef
+    })
+
     return (
-        <header className='absolute p-4 w-screen z-1 justify-between text-white grid grid-cols-3'>
-            <Link href="/" className='col-start-2 col-end-3 uppercase font-bold text-2xl text-center'>Chemicals</Link>
+        <header ref={containerRef} className='absolute p-4 w-screen z-1 justify-between text-white grid grid-cols-3'>
+            <Link data-main-link href="/" className='col-start-2 col-end-3 uppercase font-bold text-2xl text-center opacity-0'>Chemicals</Link>
             <nav className='self-center col-start-3 col-end-4 text-right'>
                 {NAV_LINKS.map((link, i) => (
                     <React.Fragment key={link.href}>
                         <Link href={link.href}
-                            className={`link ${pathname === link.href ? 'underline' : ''} hover:opacity-60 transition`}
+                            className={`link ${pathname === link.href ? 'underline' : ''} hover:opacity-60! transition opacity-0`}
                         >
                             {link.label}
                         </Link>
-                        {i < NAV_LINKS.length - 1 && <span>, </span>}
+                        {i < NAV_LINKS.length - 1 && <span className='opacity-0'>, </span>}
                     </React.Fragment>
 
                 ))}
